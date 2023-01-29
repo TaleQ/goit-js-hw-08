3a505d02d615bbddec6d9fe2c3419d3d92fa02f1
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-01-29 23:34:34 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,12 +3,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
 
-// creating and rendering of markup
 const galleryMarkup = galleryItems
   .map(({ preview, original, description }) => {
     return `<a class="gallery__item" href="${original}">
@@ -22,7 +18,6 @@ const galleryMarkup = galleryItems
   .join("");
 gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-// adding lightboxes
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionPosition: "bottom",
